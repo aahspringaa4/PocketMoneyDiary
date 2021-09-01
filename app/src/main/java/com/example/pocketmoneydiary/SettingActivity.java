5c3456f28729d649a69038ccc6ec922833108533
aahspringaa4 <aahspringaa4@naver.com>
2021-09-01 22:32:23 +0900

    [Feat] 초기화 버튼 구현

--- app/src/main/java/com/example/pocketmoneydiary/SettingActivity.java
+++ app/src/main/java/com/example/pocketmoneydiary/SettingActivity.java
@@ -3,12 +3,35 @@ package com.example.pocketmoneydiary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class SettingActivity extends AppCompatActivity {
 
+    ImageButton back;
+    Button clear;
+    public static int i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        back = (ImageButton)findViewById(R.id.back);
+        clear = (Button)findViewById(R.id.clear);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.tempBoolean3 = true;
+            }
+        });
     }
 }
