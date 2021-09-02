53887b79d499e7c1d8c512c8d1d31008425f53da
aahspringaa4 <aahspringaa4@naver.com>
2021-09-02 19:06:19 +0900

    [Feat] RecyclerView Adapter Class 추가

---
+++ app/src/main/java/com/example/pocketmoneydiary/MemoAdapter.java
@@ -0,0 +1,27 @@
+package com.example.pocketmoneydiary;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+public class MemoAdapter extends RecyclerView.Adapter {
+    @NonNull
+    @NotNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
