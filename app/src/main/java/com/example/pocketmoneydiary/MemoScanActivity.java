796f4822b3adac1f1a2c78015e463fe6baa6d3f5
aahspringaa4 <aahspringaa4@naver.com>
2021-09-03 09:31:00 +0900

    [Feat] 상단 바 기능 추가 & back 기능 추가

--- app/src/main/java/com/example/pocketmoneydiary/MemoScanActivity.java
+++ app/src/main/java/com/example/pocketmoneydiary/MemoScanActivity.java
@@ -3,12 +3,29 @@ package com.example.pocketmoneydiary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MemoScanActivity extends AppCompatActivity {
+    TextView topic;
+    ImageButton back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memo_scan);
+
+        back = (ImageButton)findViewById(R.id.back);
+        topic = (TextView)findViewById(R.id.textView2);
+        back.bringToFront();
+        topic.bringToFront();
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
