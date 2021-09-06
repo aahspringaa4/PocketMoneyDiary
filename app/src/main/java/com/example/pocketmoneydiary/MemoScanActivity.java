b38aa6139d839aec22f56dcc07b7f3d86c4ef043
aahspringaa4 <aahspringaa4@naver.com>
2021-09-06 11:14:02 +0900

    [Feat] 달력 Dialog 기능 추가 및 기타 기능 구현

--- app/src/main/java/com/example/pocketmoneydiary/MemoScanActivity.java
+++ app/src/main/java/com/example/pocketmoneydiary/MemoScanActivity.java
@@ -2,15 +2,20 @@ package com.example.pocketmoneydiary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MemoScanActivity extends AppCompatActivity {
     TextView topic;
     ImageButton back;
-
+    Button nowaday;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +23,7 @@ public class MemoScanActivity extends AppCompatActivity {
 
         back = (ImageButton)findViewById(R.id.back);
         topic = (TextView)findViewById(R.id.textView2);
+        nowaday = (Button)findViewById(R.id.nowaday);
         back.bringToFront();
         topic.bringToFront();
 
@@ -25,6 +31,29 @@ public class MemoScanActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        nowaday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar c = Calendar.getInstance();
+                int nYear = c.get(Calendar.YEAR);
+                int nMon = c.get(Calendar.MONTH);
+                int nDay = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog.OnDateSetListener mDateSetListener =
+                        new DatePickerDialog.OnDateSetListener() {
+                            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                                  int dayOfMonth) {
+                                nowaday.setText(String.valueOf(year) + "년 " + String.valueOf(monthOfYear + 1)+ "월 " + String.valueOf(dayOfMonth )+ "일");
+                            }
+                        };
+
+                DatePickerDialog oDialog = new DatePickerDialog(MemoScanActivity.this,
+                        android.R.style.Theme_DeviceDefault_Light_Dialog,
+                        mDateSetListener, nYear, nMon, nDay);
+                oDialog.show();
             }
         });
     }
