188bc798a47e99d71d9b25d2b0c9490f86c67e9f
aahspringaa4 <aahspringaa4@naver.com>
2021-09-09 16:56:21 +0900

    [Feat] 메모장 형식 변경

--- app/src/main/java/com/example/pocketmoneydiary/MemoActivity.java
+++ app/src/main/java/com/example/pocketmoneydiary/MemoActivity.java
@@ -25,10 +25,10 @@ public class MemoActivity extends AppCompatActivity {
     TextView topic;
     ImageButton back, go;
     private int REQUEST_TEST = 200;
-    private ArrayList<MemoData> arrayList; // MaindData를 담은 배열 리스트인 arrayList를 선언
-    private MemoAdapter memoAdapter; // 인스턴스 mainAdapter를 선언
+    private ArrayList<String> arrayList; // MaindData를 담은 배열 리스트인 arrayList를 선언
+    private static MemoAdapter adapter; // 인스턴스 mainAdapter를 선언
     private RecyclerView recyclerView;
-    private LinearLayoutManager linearLayoutManager; // 레이아웃 관리자 선언
+    private LinearLayoutManager LinearLayoutManager; // 레이아웃 관리자 선언
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +36,11 @@ public class MemoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_memo);
 
         recyclerView = (RecyclerView) findViewById(R.id.rv);
-        linearLayoutManager = new LinearLayoutManager(this);
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(linearLayoutManager); // recyclerView를 통해 리사이클러뷰를 지정
-        memoAdapter = new MemoAdapter(arrayList); // 인스턴스
-        recyclerView.setAdapter(memoAdapter); // 어댑터를 mainAdapter로
-        arrayList = new ArrayList<>();
+        adapter = new MemoAdapter(arrayList); // 인스턴스
+        recyclerView.setAdapter(adapter); // 어댑터를 mainAdapter로
+        arrayList = new ArrayList<String>();
 
         go = (ImageButton) findViewById(R.id.go);
         back = (ImageButton) findViewById(R.id.back);
@@ -59,58 +59,12 @@ public class MemoActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MemoScanActivity.class);
-                //startActivity(intent); // 해당 액티비티에 단순 값 전달과 이동이면 startActivity
-                startActivityForResult(intent, REQUEST_TEST); // 메모를 작성하고 결과값을 받기 위해
+                startActivity(intent);
             }
         });
-
-        SharedPreferences prefb = getSharedPreferences("save2", MODE_PRIVATE);
-        Collection<?> col_val = prefb.getAll().values();
-        Iterator<?> it_val = col_val.iterator();
-        Collection<?> col_key = prefb.getAll().keySet();
-        Iterator<?> it_key = col_key.iterator();
-
-        while (it_val.hasNext() && it_key.hasNext()) {
-            String key = (String) it_key.next();
-            String value = (String) it_val.next();
-
-            // value 값은 다음과 같이 저장되어있다
-            // "{\"title\":\"hi title\",\"content\":\"hi content\"}"
-            try {
-                // String으로 된 value를 JSONObject로 변환하여 key-value로 데이터 추출
-                JSONObject jsonObject = new JSONObject(value);
-                String day = (String) jsonObject.getString("get");
-                String content = (String) jsonObject.getString("memo2");
-                // 리사이클러뷰 어뎁터 addItem으로 목록 추가
-                memoAdapter.addItem(new MemoData(day, content));
-            } catch (JSONException e) {
-
-            }
-
-            // 목록 갱신하여 뷰에 띄어줌
-            memoAdapter.notifyDataSetChanged();
-        }
-
     }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == REQUEST_TEST) {
-            if (resultCode == RESULT_OK) {
-                // 전달 받은 값
-                Intent intent = getIntent();
-                String get_Tv_today = data.getStringExtra("get");
-                String get_content = data.getStringExtra("memo2");
-                // 리사이클러뷰 목록에 추가
-                memoAdapter.addItem(new MemoData(get_Tv_today,get_content));
-                // 목록 갱신
-                memoAdapter.notifyDataSetChanged();
-                Toast.makeText(MemoActivity.this, "메모 작성 되었습니다", Toast.LENGTH_SHORT).show();
-            } else {   // RESULT_CANCEL
-                Toast.makeText(MemoActivity.this, "취소", Toast.LENGTH_SHORT).show();
-            }
-
-        }
+    public static void Scan() {
+        adapter.items.add(Integer.parseInt(MemoScanActivity.str), MemoScanActivity.stt);
     }
 }
