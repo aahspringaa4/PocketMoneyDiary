9792e4313e828489faa94f529c60c7cb04d2e05d
aahspringaa4 <aahspringaa4@naver.com>
2021-09-09 15:47:24 +0900

    [Feat] SharedPref 값 받아오기

--- app/src/main/java/com/example/pocketmoneydiary/MemoActivity.java
+++ app/src/main/java/com/example/pocketmoneydiary/MemoActivity.java
@@ -5,13 +5,20 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 
 public class MemoActivity extends AppCompatActivity {
 
@@ -35,12 +42,12 @@ public class MemoActivity extends AppCompatActivity {
         recyclerView.setAdapter(memoAdapter); // 어댑터를 mainAdapter로
         arrayList = new ArrayList<>();
 
-        go = (ImageButton)findViewById(R.id.go);
-        back = (ImageButton)findViewById(R.id.back);
-        topic = (TextView)findViewById(R.id.textView2);
+        go = (ImageButton) findViewById(R.id.go);
+        back = (ImageButton) findViewById(R.id.back);
+        topic = (TextView) findViewById(R.id.textView2);
         back.bringToFront();
         topic.bringToFront();
-        
+
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,27 +60,57 @@ public class MemoActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MemoScanActivity.class);
                 //startActivity(intent); // 해당 액티비티에 단순 값 전달과 이동이면 startActivity
-                startActivityForResult(intent,REQUEST_TEST) ; // 메모를 작성하고 결과값을 받기 위해
+                startActivityForResult(intent, REQUEST_TEST); // 메모를 작성하고 결과값을 받기 위해
             }
         });
+
+        SharedPreferences prefb = getSharedPreferences("save2", MODE_PRIVATE);
+        Collection<?> col_val = prefb.getAll().values();
+        Iterator<?> it_val = col_val.iterator();
+        Collection<?> col_key = prefb.getAll().keySet();
+        Iterator<?> it_key = col_key.iterator();
+
+        while (it_val.hasNext() && it_key.hasNext()) {
+            String key = (String) it_key.next();
+            String value = (String) it_val.next();
+
+            // value 값은 다음과 같이 저장되어있다
+            // "{\"title\":\"hi title\",\"content\":\"hi content\"}"
+            try {
+                // String으로 된 value를 JSONObject로 변환하여 key-value로 데이터 추출
+                JSONObject jsonObject = new JSONObject(value);
+                String day = (String) jsonObject.getString("get");
+                String content = (String) jsonObject.getString("memo2");
+                // 리사이클러뷰 어뎁터 addItem으로 목록 추가
+                memoAdapter.addItem(new MemoData(day, content));
+            } catch (JSONException e) {
+
+            }
+
+            // 목록 갱신하여 뷰에 띄어줌
+            memoAdapter.notifyDataSetChanged();
+        }
+
     }
-
-    public static void memo() {
-
-//        adapter.array.add(getText);
-//        adapter.notifyDataSetChanged();
-//        getTextET.setText("");
-    }
-
     @Override
-    protected void onResume() {
-        super.onResume();
-        itemadd();
-    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-    private void itemadd() {
-        MemoData MemoData;
+        if (requestCode == REQUEST_TEST) {
+            if (resultCode == RESULT_OK) {
+                // 전달 받은 값
+                Intent intent = getIntent();
+                String get_Tv_today = data.getStringExtra("get");
+                String get_content = data.getStringExtra("memo2");
+                // 리사이클러뷰 목록에 추가
+                memoAdapter.addItem(new MemoData(get_Tv_today,get_content));
+                // 목록 갱신
+                memoAdapter.notifyDataSetChanged();
+                Toast.makeText(MemoActivity.this, "메모 작성 되었습니다", Toast.LENGTH_SHORT).show();
+            } else {   // RESULT_CANCEL
+                Toast.makeText(MemoActivity.this, "취소", Toast.LENGTH_SHORT).show();
+            }
 
-        memoAdapter.notifyDataSetChanged(); // 새로고침
+        }
     }
 }
