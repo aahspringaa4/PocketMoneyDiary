28651f618a4acece0de3f3fd25ec15dd4e826e8b
aahspringaa4 <aahspringaa4@naver.com>
2021-09-13 22:50:41 +0900

    [Feat] 게시된 게시물 삭제 기능 추가

--- app/src/main/java/com/example/pocketmoneydiary/MemoAdapter.java
+++ app/src/main/java/com/example/pocketmoneydiary/MemoAdapter.java
@@ -2,6 +2,7 @@ package com.example.pocketmoneydiary;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -9,11 +10,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import org.jetbrains.annotations.NotNull;
@@ -23,7 +26,7 @@ import java.net.CookieStore;
 import java.util.ArrayList;
 
 public class MemoAdapter extends RecyclerView.Adapter<MemoAdapter.CustomViewHolder> {
-    public ArrayList mcontext;
+    public ArrayList<String> mcontext = null;
     private Activity Mc;
 
     ArrayList<String> items = new ArrayList<String>(); // MainData를 리스트배열 arraylist에 넣음
@@ -63,6 +66,13 @@ public class MemoAdapter extends RecyclerView.Adapter<MemoAdapter.CustomViewHold
                 Mc.startActivity(intent);
             }
         });
+        holder.delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                remove(position);
+            }
+        });
+
     }
 
     @Override
@@ -70,25 +80,25 @@ public class MemoAdapter extends RecyclerView.Adapter<MemoAdapter.CustomViewHold
         return (null != items ? items.size() : 0);
     } // arraylist가 null이 아닐 경우 arraylist.size() 반환
 
-//    public void remove(int position) {
-//        try { // 예외사항이 생겨도 강제실행
-//            arraylist.remove(position);
-//            notifyItemRemoved(position); // 새로고침
-//        } catch (IndexOutOfBoundsException ex) {
-//            ex.printStackTrace();
-//        }
-//    }
+    public void remove(int position) {
+        items.remove(position);
+        item.remove(position);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(position, items.size());
+        notifyItemRangeChanged(position, item.size());
+    }
 
     public class CustomViewHolder extends RecyclerView.ViewHolder {
         protected TextView tv_content;
         protected TextView tv_today;
         protected TextView patch;
-
+        protected TextView delete;
         public CustomViewHolder(@NonNull View itemView) {
             super(itemView);
             this.tv_today = (TextView) itemView.findViewById(R.id.today);
             this.tv_content = (TextView) itemView.findViewById(R.id.content);
             this.patch = (TextView) itemView.findViewById(R.id.patch);
+            this.delete = (TextView) itemView.findViewById(R.id.delete);
         }
     }
 }
