d8b5054a41e98c41394475537f8bf87a90caa091
aahspringaa4 <aahspringaa4@naver.com>
2021-09-19 21:58:45 +0900

    [Feat] 설정 기능 추가

--- app/src/main/java/com/example/pocketmoneydiary/SettingActivity.java
+++ app/src/main/java/com/example/pocketmoneydiary/SettingActivity.java
@@ -12,13 +12,14 @@ public class SettingActivity extends AppCompatActivity {
     ImageButton back;
     Button clear;
     public static int i = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
-        back = (ImageButton)findViewById(R.id.back);
-        clear = (Button)findViewById(R.id.clear);
+        back = (ImageButton) findViewById(R.id.back);
+        clear = (Button) findViewById(R.id.clear);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
