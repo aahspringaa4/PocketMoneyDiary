efc8e5364852cd0d756c6a5ae52a017515060d1a
aahspringaa4 <aahspringaa4@naver.com>
2021-09-25 15:46:14 +0900

    [Feat] 공휴일 달력 생성

--- app/src/main/java/com/example/pocketmoneydiary/calender/EventDecorator.java
+++ app/src/main/java/com/example/pocketmoneydiary/calender/EventDecorator.java
@@ -1,2 +1,37 @@
-package com.example.pocketmoneydiary.calender;public class EventDecorator {
+package com.example.pocketmoneydiary.calender;
+
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
+
+
+import com.example.pocketmoneydiary.R;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import java.util.Collection;
+import java.util.HashSet;
+
+public class EventDecorator implements DayViewDecorator {
+
+    private final Drawable drawable;
+    private int color;
+    private HashSet<CalendarDay> dates;
+
+    public EventDecorator(int color, Collection<CalendarDay> dates, Activity context) {
+        drawable = context.getResources().getDrawable(R.drawable.more);
+        this.color = color;
+        this.dates = new HashSet<>(dates);
+    }
+
+    @Override
+    public boolean shouldDecorate(CalendarDay day) {
+        return dates.contains(day);
+    }
+
+    @Override
+    public void decorate(DayViewFacade view) {
+        view.setSelectionDrawable(drawable);
+        //view.addSpan(new DotSpan(5, color)); // 날자밑에 점
+    }
 }
+
