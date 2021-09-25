619682db141c5fa38ddd90454cf3ad64eb5c6a5e
aahspringaa4 <aahspringaa4@naver.com>
2021-09-25 22:18:18 +0900

    [Feat] 도움말 TextVIew 스크롤 기능 추가

--- app/src/main/java/com/example/pocketmoneydiary/ButtomFragment/HelpActivity.java
+++ app/src/main/java/com/example/pocketmoneydiary/ButtomFragment/HelpActivity.java
@@ -3,15 +3,20 @@ package com.example.pocketmoneydiary.ButtomFragment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 import com.example.pocketmoneydiary.R;
 
 public class HelpActivity extends AppCompatActivity {
 
+    TextView tv_help;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
 
+        tv_help = findViewById(R.id.help);
+        tv_help.setMovementMethod(new ScrollingMovementMethod());
     }
 }
