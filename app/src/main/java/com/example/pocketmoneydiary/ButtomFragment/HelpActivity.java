e04bfbcad8bf8fa8da2c782569e6d16942fd10a8
aahspringaa4 <aahspringaa4@naver.com>
2021-09-25 21:35:43 +0900

    [Feat] 도움 화면 클라이언트 구현

--- app/src/main/java/com/example/pocketmoneydiary/ButtomFragment/HelpActivity.java
+++ app/src/main/java/com/example/pocketmoneydiary/ButtomFragment/HelpActivity.java
@@ -1,8 +1,10 @@
-package com.example.pocketmoneydiary;
+package com.example.pocketmoneydiary.ButtomFragment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.pocketmoneydiary.R;
 
 public class HelpActivity extends AppCompatActivity {
 
